e000181207be56d9744b2b80ce03bad48daaa1d5
vBoBv <vannponhvath@gmail.com>
2020-09-01 23:56:02 +1200

    Setup Customer Model extended from django user model

--- Assignment/iscg7420-Assignment1/spaceout/ecommerce_app/models.py
+++ Assignment/iscg7420-Assignment1/spaceout/ecommerce_app/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
-# Create your models here.
+
+class CustomerManager(BaseUserManager):
+
+    def create_customer(self, email, password=None, **other_fields):
+
+        customer = self.model(email=self.normalize_email(email), **other_fields)
+        customer.set_password(password)
+        customer.save(using=self._db)
+
+        return customer
+
+
+class Customer(AbstractBaseUser, PermissionsMixin):
+
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255, unique=True)
+    phone_number = models.IntegerField(max_length=20)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = CustomerManager()
+
+    USERNAME_FIELD = 'email'
