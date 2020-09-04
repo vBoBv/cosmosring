daa2227e92bec5fb8e2a9daffac93553dbb7d4bd
vBoBv <vannponhvath@gmail.com>
2020-09-05 00:50:36 +1200

    added 10 basic views

--- Assignment/iscg7420-Assignment1/spaceout/ecommerce_app/views.py
+++ Assignment/iscg7420-Assignment1/spaceout/ecommerce_app/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'home')
+
+
+def signup(request):
+    return render(request, 'signup')
+
+
+def signin(request):
+    return render(request, 'signin')
+
+
+def resetpassword(request):
+    return render(request, 'resetpassword')
+
+
+def products(request):
+    return render(request, 'products')
+
+
+def checkout(request):
+    return render(request, 'checkout')
+
+
+def bid(request):
+    return render(request, 'bid')
+
+
+def trade(request):
+    return render(request, 'trade')
+
+
+def product(request):
+    return render(request, 'product')
+
+
+def ordersummary(request):
+    return render(request, 'ordersummary')
