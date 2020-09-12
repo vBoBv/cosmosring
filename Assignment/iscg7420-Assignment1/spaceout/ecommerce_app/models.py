655875df4812a3fb94b218e82e378a3ed113ff4d
vBoBv <vannponhvath@gmail.com>
2020-09-12 19:44:33 +1200

    added __str__ to OrderManager

--- Assignment/iscg7420-Assignment1/spaceout/ecommerce_app/models.py
+++ Assignment/iscg7420-Assignment1/spaceout/ecommerce_app/models.py
@@ -74,6 +74,9 @@ class Customer(models.Model):
 class OrderManager(models.Model):
     user = models.OneToOneField('User', on_delete=models.CASCADE, primary_key=True)
 
+    def __str__(self):
+        return self.user
+
 
 class Payment(models.Model):
     amount = models.FloatField()
