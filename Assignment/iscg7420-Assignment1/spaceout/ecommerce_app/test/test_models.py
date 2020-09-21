05d80a22d71d580bdab0d203c7cea319d530c92b
vBoBv <vannponhvath@gmail.com>
2020-09-21 23:51:59 +1200

    used test fixtures

--- Assignment/iscg7420-Assignment1/spaceout/ecommerce_app/test/test_models.py
+++ Assignment/iscg7420-Assignment1/spaceout/ecommerce_app/test/test_models.py
@@ -1,8 +1,9 @@
 from django.test import TestCase
-from ecommerce_app.models import Product, Category, Discount
+from ecommerce_app.models import Product, Category, Discount, Address
 
 
 class TestModels(TestCase):
+    fixtures = ['address.json', 'discount.json', 'category.json']
 
     def setUp(self):
         self.category_planet = Category.objects.create(
@@ -29,3 +30,21 @@ class TestModels(TestCase):
     def test_discount_is_created(self):
         self.assertEquals(self.discount.discount_code, 'billclient')
         self.assertEquals(self.discount.is_expired, False)
+
+    def test_addresses_is_existed(self):
+        addresses = Address.objects.filter(city='AKL')
+        self.assertEquals(addresses.first().city, 'AKL')
+        self.assertEquals(addresses.last().city, 'AKL')
+
+    def test_discounts_is_expired(self):
+        discounts = Discount.objects.filter(is_expired=True)
+        self.assertEquals(discounts.first().is_expired, True)
+        self.assertEquals(discounts.last().is_expired, True)
+
+    def test_category1_is_planet(self):
+        category = Category.objects.get(id=1)
+        self.assertEquals(category.name, 'Planet')
+
+    def test_category2_is_asteroid(self):
+        category = Category.objects.get(id=2)
+        self.assertEquals(category.name, 'Asteroid')
