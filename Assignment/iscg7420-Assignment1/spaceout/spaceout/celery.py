4091c1410140b27ecfd7fb7b20bbd78f5849572f
vBoBv <vannponhvath@gmail.com>
2020-09-24 03:14:56 +1200

    updated celery setting

--- Assignment/iscg7420-Assignment1/spaceout/spaceout/celery.py
+++ Assignment/iscg7420-Assignment1/spaceout/spaceout/celery.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import, unicode_literals
 import os
 
 from celery import Celery
+from django.conf import settings
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'spaceout.settings')
@@ -16,7 +17,7 @@ app = Celery('spaceout')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Load task modules from all registered Django app configs.
-app.autodiscover_tasks()
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
 
 @app.task(bind=True)
