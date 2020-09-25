e7d16d5e5035d490a46ec29bd9c70c99d4a17a44
vBoBv <vannponhvath@gmail.com>
2020-09-26 00:03:45 +1200

    updated allowed hosts

--- Assignment/iscg7420-Assignment1/spaceout/spaceout/settings.py
+++ Assignment/iscg7420-Assignment1/spaceout/spaceout/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'glacial-thicket-14145.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'glacial-thicket-14145.herokuapp.com', 'www.cosmosring.me', 'cosmosring.me']
 
 # Application definition
 
