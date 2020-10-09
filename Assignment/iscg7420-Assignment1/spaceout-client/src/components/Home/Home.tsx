4813b80facda3f7c6915374353d15b46912835c3
vBoBv <vannponhvath@gmail.com>
2020-10-09 16:34:52 +1300

    tweaked Home

--- Assignment/iscg7420-Assignment1/spaceout-client/src/components/Home/Home.tsx
+++ Assignment/iscg7420-Assignment1/spaceout-client/src/components/Home/Home.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Grid } from '@material-ui/core';
+import { useStyles } from './HomeCSS';
+import StarParticles from '../StarParticles/StarParticles';
 
 const Home = () => {
-	return <div>Home</div>;
+	const { containerBackground } = useStyles();
+
+	return (
+		<Grid container className={containerBackground} direction='column'>
+			<StarParticles />
+		</Grid>
+	);
 };
 
 export default Home;
