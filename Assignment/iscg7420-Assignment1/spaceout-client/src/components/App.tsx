fb5a46fc9c04f5ced22ec7c1baf11f57dd4cdd82
vBoBv <vannponhvath@gmail.com>
2020-10-09 16:35:10 +1300

    added Home and CssBaseline to App

--- Assignment/iscg7420-Assignment1/spaceout-client/src/components/App.tsx
+++ Assignment/iscg7420-Assignment1/spaceout-client/src/components/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import Home from './Home/Home';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 const App = () => {
-	return <div>App</div>;
+	return (
+		<div>
+			<CssBaseline />
+			<Home />
+		</div>
+	);
 };
 
 export default App;
