f6e2ac897cbc5b9a39b1df291dabf719e1748f44
vBoBv <vannponhvath@gmail.com>
2020-10-09 18:27:52 +1300

    added NavigationBar component

---
+++ Assignment/iscg7420-Assignment1/spaceout-client/src/components/NavigationBar/NavigationBar.tsx
@@ -0,0 +1,39 @@
+import React, { useEffect } from 'react';
+import { useStyles } from './NavigationBarCSS';
+import { Grid } from '@material-ui/core';
+import Typed from 'react-typed';
+import WOW from 'wowjs';
+
+import logoImg from '../../assets/logo.png';
+
+const NavigationBar = () => {
+	useEffect(() => {
+		new WOW.WOW({
+			live: false
+		}).init();
+	}, []);
+
+	const { navigationBarContainer, logoContainer, logo, logoText } = useStyles();
+
+	return (
+		<Grid container className={navigationBarContainer} direction='row' justify='space-between' alignItems='center'>
+			<Grid item className={logoContainer}>
+				<Grid item>
+					<img
+						className={`${logo} wow rollIn rotatingAnimation`}
+						src={logoImg}
+						alt='Logo'
+						data-wow-duration='2s'
+						data-wow-delay='1.5s'
+					/>
+				</Grid>
+				<Grid item className={logoText}>
+					<Typed strings={['Above']} typeSpeed={150} backSpeed={100} backDelay={10} smartBackspace />
+				</Grid>
+			</Grid>
+			<Grid item>Links</Grid>
+		</Grid>
+	);
+};
+
+export default NavigationBar;
