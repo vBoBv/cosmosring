8f4634a7dab4f650c936bad5f72acaa7a33deacb
vBoBv <vannponhvath@gmail.com>
2020-10-10 21:29:02 +1300

    added CSS to HomeCSS

--- Assignment/iscg7420-Assignment1/spaceout-client/src/components/Home/HomeCSS.ts
+++ Assignment/iscg7420-Assignment1/spaceout-client/src/components/Home/HomeCSS.ts
@@ -3,18 +3,48 @@ import homeBackground from '../../assets/space.jpg';
 
 export const useStyles = makeStyles((theme) => ({
 	containerBackground: {
+		position: 'relative',
 		backgroundImage: `url(${homeBackground})`,
-		backgroundPosition: 'center',
+		backgroundPosition: 'top',
 		backgroundSize: 'cover',
 		backgroundAttachment: 'fixed',
 		backgroundRepeat: 'no-repeat',
 		height: '100vh'
 	},
-	starParticles: {
-		position: 'absolute',
-		top: 0,
-		left: 0,
-		height: '100vh',
-		width: '100%'
+	secondBackground: {
+		position: 'relative',
+		backgroundColor: 'red',
+		height: '100vh'
+	},
+	headingContainer: {
+		zIndex: 1,
+		color: 'white',
+		height: '100%'
+	},
+	pageSubHeading1: {
+		fontFamily: 'Bungee Hairline, cursive',
+		fontSize: '3rem',
+		[theme.breakpoints.down('md')]: {
+			fontSize: '2.5rem'
+		},
+		[theme.breakpoints.down('sm')]: {
+			fontSize: '1.8rem'
+		},
+		[theme.breakpoints.down('xs')]: {
+			fontSize: '1.4rem'
+		}
+	},
+	pageHeading: {
+		fontFamily: 'Nova Cut, cursive',
+		fontSize: '5rem',
+		[theme.breakpoints.down('md')]: {
+			fontSize: '3rem'
+		},
+		[theme.breakpoints.down('sm')]: {
+			fontSize: '2rem'
+		},
+		[theme.breakpoints.down('xs')]: {
+			fontSize: '1.5rem'
+		}
 	}
 }));
