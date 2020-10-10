8591f1169ef60a9c7025125cb87e6b5cca9a2c4e
vBoBv <vannponhvath@gmail.com>
2020-10-10 21:28:34 +1300

    added ReactPageScroller to Home

--- Assignment/iscg7420-Assignment1/spaceout-client/src/components/Home/Home.tsx
+++ Assignment/iscg7420-Assignment1/spaceout-client/src/components/Home/Home.tsx
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Grid } from '@material-ui/core';
 import { useStyles } from './HomeCSS';
+import ReactPageScroller from 'react-page-scroller';
+import HomeLandingContent from './HomeLandingContent';
+import HomeLandingContent2 from './HomeLandingContent2';
 import StarParticles from '../StarParticles/StarParticles';
 import NavigationBar from '../NavigationBar/NavigationBar';
 
 const Home = () => {
 	const { containerBackground } = useStyles();
+	const [currentPage, setCurrentPage] = useState<number>(0);
+
+	const handlePageChange = (pageNumber: number) => setCurrentPage(pageNumber);
 
 	return (
 		<Grid container className={containerBackground} direction='column'>
-			<StarParticles />
 			<NavigationBar />
+			<Grid container style={{ zIndex: 1 }}>
+				<ReactPageScroller
+					containerWidth='100vw'
+					containerHeight='90vh'
+					pageOnChange={handlePageChange}
+					customPageNumber={currentPage}>
+					<HomeLandingContent />
+					<HomeLandingContent2 />
+				</ReactPageScroller>
+			</Grid>
+			<StarParticles />
 		</Grid>
 	);
 };
