75e2d2b1321e3be263ec228b4003af8824a43f87
vBoBv <vannponhvath@gmail.com>
2020-10-12 23:56:16 +1300

    tweaked HomeLandingAstronaut

--- Assignment/iscg7420-Assignment1/spaceout-client/src/components/Home/HomeLandingAstronaut.tsx
+++ Assignment/iscg7420-Assignment1/spaceout-client/src/components/Home/HomeLandingAstronaut.tsx
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import { Grid, Typography } from '@material-ui/core';
+import React, { useEffect } from 'react';
+import { Button, Grid, Typography, Chip } from '@material-ui/core';
 import { useStyles } from './HomeCSS';
 import WOW from 'wowjs';
+import FingerprintIcon from '@material-ui/icons/Fingerprint';
 
 import astronaut from '../../assets/astronaut.png';
 import pluto from '../../assets/pluto.png';
@@ -12,7 +13,8 @@ interface IHomeLandingAstronaut {
 }
 
 const HomeLandingAstronaut: React.FC<IHomeLandingAstronaut> = ({ pageNumber }) => {
-	const { headingContainer, newDawnHeading, newDawn, newDawnImgItem } = useStyles();
+	const { headingContainer, newDawnHeading, newDawn, newDawnImgItem, buttonStyle } = useStyles();
+	const buttonIcon = 35;
 
 	useEffect(() => {
 		new WOW.WOW({
@@ -27,37 +29,58 @@ const HomeLandingAstronaut: React.FC<IHomeLandingAstronaut> = ({ pageNumber }) =
 					It's a new dawn
 				</Typography>
 			</Grid>
-			<Grid item lg={4} className={newDawn}>
-				<img
-					src={pageNumber === 1 ? astronaut : ''}
-					className={`${newDawnImgItem} ${pageNumber === 1 ? 'wow fadeInLeft animated animated' : null}`}
-					alt={pageNumber === 1 ? 'Astronaut' : ''}
-					style={pageNumber === 1 ? { visibility: 'visible', animationDuration: '2s', animationDelay: '0.5s' } : {}}
-				/>
-			</Grid>
-			<Grid item lg={4} className={newDawn}>
-				<img
-					src={pageNumber === 1 ? pluto : ''}
-					className={`${newDawnImgItem} ${pageNumber === 1 ? 'wow rotateIn animated animated' : null}`}
-					alt={pageNumber === 1 ? 'Planet' : ''}
-					style={pageNumber === 1 ? { visibility: 'visible', animationDuration: '2s', animationDelay: '0.5s' } : {}}
-					// data-wow-duration='2s'
-					// data-wow-delay='0.5s'
-					// src={pluto}
-					// className={`${newDawnImgItem} wow rotateIn`}
-					// alt='Planet'
-					// data-wow-duration='2s'
-					// data-wow-delay='0.5s'
-				/>
-			</Grid>
-			<Grid item lg={4} className={newDawn}>
-				<img
-					src={pageNumber === 1 ? galaxy : ''}
-					className={`${newDawnImgItem} ${pageNumber === 1 ? 'wow fadeInRight animated animated' : null}`}
-					alt={pageNumber === 1 ? 'Galaxy' : ''}
-					style={pageNumber === 1 ? { visibility: 'visible', animationDuration: '2s', animationDelay: '0.5s' } : {}}
-				/>
-			</Grid>
+			{pageNumber === 1 ? (
+				<>
+					<Grid item lg={3} className={newDawn}>
+						<img
+							src={pageNumber === 1 ? astronaut : ''}
+							className={`${newDawnImgItem} ${pageNumber === 1 ? 'wow fadeInLeft animated animated' : null}`}
+							alt={pageNumber === 1 ? 'Astronaut' : ''}
+							style={pageNumber === 1 ? { visibility: 'visible', animationDuration: '2s', animationDelay: '0.3s' } : {}}
+						/>
+						<Button
+							variant='outlined'
+							color='inherit'
+							startIcon={<FingerprintIcon style={{ fontSize: buttonIcon }} />}
+							className={`${buttonStyle} ${pageNumber === 1 ? 'wow fadeInUpBig animated animated' : null}`}
+							style={pageNumber === 1 ? { visibility: 'visible', animationDuration: '2s', animationDelay: '1s' } : {}}>
+							Get to know us
+						</Button>
+					</Grid>
+					<Grid item lg={3} className={newDawn}>
+						<img
+							src={pageNumber === 1 ? pluto : ''}
+							className={`${newDawnImgItem} ${pageNumber === 1 ? 'wow rotateIn animated animated' : null}`}
+							alt={pageNumber === 1 ? 'Planet' : ''}
+							style={pageNumber === 1 ? { visibility: 'visible', animationDuration: '2s', animationDelay: '0.3s' } : {}}
+						/>
+						<Button
+							variant='outlined'
+							color='inherit'
+							startIcon={<FingerprintIcon style={{ fontSize: buttonIcon }} />}
+							className={`${buttonStyle} ${pageNumber === 1 ? 'wow fadeInUpBig animated animated' : null}`}
+							style={pageNumber === 1 ? { visibility: 'visible', animationDuration: '2s', animationDelay: '1s' } : {}}>
+							Browse
+						</Button>
+					</Grid>
+					<Grid item lg={3} className={newDawn}>
+						<img
+							src={pageNumber === 1 ? galaxy : ''}
+							className={`${newDawnImgItem} ${pageNumber === 1 ? 'wow fadeInRight animated animated' : null}`}
+							alt={pageNumber === 1 ? 'Galaxy' : ''}
+							style={pageNumber === 1 ? { visibility: 'visible', animationDuration: '2s', animationDelay: '0.3s' } : {}}
+						/>
+						<Button
+							variant='outlined'
+							color='inherit'
+							startIcon={<FingerprintIcon style={{ fontSize: buttonIcon }} />}
+							className={`${buttonStyle} ${pageNumber === 1 ? 'wow fadeInUpBig animated animated' : null}`}
+							style={pageNumber === 1 ? { visibility: 'visible', animationDuration: '2s', animationDelay: '1s' } : {}}>
+							Become a Seller
+						</Button>
+					</Grid>
+				</>
+			) : null}
 		</Grid>
 	);
 };
