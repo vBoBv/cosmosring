37e01d8adafd131e1f5740bbe6d46d30a101bced
vBoBv <vannponhvath@gmail.com>
2020-10-14 15:14:47 +1300

    added react-router to App

--- Assignment/iscg7420-Assignment1/spaceout-client/src/components/App.tsx
+++ Assignment/iscg7420-Assignment1/spaceout-client/src/components/App.tsx
@@ -2,13 +2,20 @@ import React from 'react';
 import Home from './Home/Home';
 import Authentication from './Authentication/Authentication';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import history from './history';
+
+import { Router, Switch, Route } from 'react-router-dom';
 
 const App = () => {
 	return (
 		<div>
 			<CssBaseline />
-			<Home />
-			<Authentication />
+			<Router history={history}>
+				<Switch>
+					<Route exact path='/' component={Home} />
+					<Route exact path='/authentication' component={Authentication} />
+				</Switch>
+			</Router>
 		</div>
 	);
 };
