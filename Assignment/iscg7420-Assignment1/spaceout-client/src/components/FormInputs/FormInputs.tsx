16deb704837b87829289dd196c331b12a2863a8f
vBoBv <vannponhvath@gmail.com>
2020-10-15 00:32:14 +1300

    cleaned up renderTextField and added renderSelectField

--- Assignment/iscg7420-Assignment1/spaceout-client/src/components/FormInputs/FormInputs.tsx
+++ Assignment/iscg7420-Assignment1/spaceout-client/src/components/FormInputs/FormInputs.tsx
@@ -1,15 +1,9 @@
 import React from 'react';
-import {
-	Grid,
-	TextField,
-	Checkbox,
-	RadioGroup,
-	Select,
-	InputBaseComponentProps,
-	InputAdornment
-} from '@material-ui/core';
+import { TextField, Select, InputBaseComponentProps, InputAdornment, InputLabel, MenuItem } from '@material-ui/core';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
 
-interface ITextFieldProps {
+interface IInputProps {
 	input: InputBaseComponentProps;
 	label: string;
 	icon: any;
@@ -19,47 +13,29 @@ interface ITextFieldProps {
 	};
 }
 
-export const renderTextField: React.FC<ITextFieldProps> = ({ input, label, icon, meta: { touched, error } }) => (
-	// <Grid container spacing={1} alignItems='flex-end'>
-	// 	<Grid item>{icon}</Grid>
-	// 	<Grid item>
-	// 		<TextField id='input-with-icon-grid' label={label}  />
-	// 	</Grid>
-	// </Grid>
+export const renderTextField: React.FC<IInputProps> = ({ input, label, icon, meta: { touched, error } }) => (
 	<TextField
-		// hintText={label}
-		// floatingLabelText={label}
-		// errorText={touched && error}
-		// {...input}
-		// {...custom}
 		label={label}
 		helperText={touched && error}
 		error={error ? true : false}
-		inputProps={
-			// startAdornment: <InputAdornment position='start'>{icon}</InputAdornment>
-			input
-		}
+		inputProps={input}
 		InputProps={{
 			startAdornment: <InputAdornment position='start'>{icon}</InputAdornment>
 		}}
 	/>
 );
 
-// export const renderCheckbox = ({ input, label }) => (
-// 	<Checkbox label={label} checked={input.value ? true : false} onCheck={input.onChange} />
-// );
-
-// export const renderRadioGroup = ({ input, ...rest }) => (
-// 	<RadioGroup {...input} {...rest} valueSelected={input.value} onChange={(event, value) => input.onChange(value)} />
-// );
-
-// export const renderSelectField = ({ input, label, meta: { touched, error }, children, ...custom }) => (
-// 	<Select
-// 		floatingLabelText={label}
-// 		errorText={touched && error}
-// 		{...input}
-// 		onChange={(event, index, value) => input.onChange(value)}
-// 		children={children}
-// 		{...custom}
-// 	/>
-// );
+export const renderSelectField: React.FC<IInputProps> = ({ input, label, meta: { touched, error } }) => (
+	<FormControl>
+		<InputLabel shrink>Country</InputLabel>
+		<Select label={label} inputProps={input} displayEmpty>
+			<MenuItem value=''>
+				<em>None</em>
+			</MenuItem>
+			<MenuItem value='Auckland'>Auckland</MenuItem>
+			<MenuItem value='Christchurch'>Christchurch</MenuItem>
+			<MenuItem value='Wellington'>Wellington</MenuItem>
+		</Select>
+		<FormHelperText>{touched && error}</FormHelperText>
+	</FormControl>
+);
