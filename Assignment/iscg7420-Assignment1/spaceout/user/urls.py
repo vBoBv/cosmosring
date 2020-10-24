44af3ea2ff17e32794cab2734173a1aba1fa116e
vBoBv <vannponhvath@gmail.com>
2020-10-25 01:41:25 +1300

    added user and token serializers and views

---
+++ Assignment/iscg7420-Assignment1/spaceout/user/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+
+from user import views
+from django.conf.urls.static import static
+from django.conf import settings
+
+app_name = 'user'
+
+urlpatterns = [
+    path('create/', views.CreateUserView.as_view(), name='create'),
+    path('token/', views.CreateTokenView.as_view(), name='token'),
+    path('profile/', views.ManageUserView.as_view(), name='profile'),
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
