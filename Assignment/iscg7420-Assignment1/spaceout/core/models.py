f0dc11562dc68bf96a9af4287a4937a5497d0a06
vBoBv <vannponhvath@gmail.com>
2020-10-24 23:57:49 +1300

    added User migration

--- Assignment/iscg7420-Assignment1/spaceout/core/models.py
+++ Assignment/iscg7420-Assignment1/spaceout/core/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    @receiver(post_save, sender=BaseUserManager)
+    def create_user(self, email, username, password=None, **other_fields):
+        if not email:
+            raise ValueError("Email is required!")
+        user = self.model(email=self.normalize_email(email), username=username, **other_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        print('User created')
+
+        return user
+
+    def create_superuser(self, email, username, password):
+        user = self.create_user(email, username, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=255, unique=True)
+    username = models.CharField(max_length=255, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_customer = models.BooleanField(default=False)
+    is_order_manager = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.email
