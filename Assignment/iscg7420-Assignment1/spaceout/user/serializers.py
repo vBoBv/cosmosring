673290d04ed8b5dd8cadc515f762cb2dfcb1d016
vBoBv <vannponhvath@gmail.com>
2020-10-28 02:44:33 +1300

    added CustomerSerializer

--- Assignment/iscg7420-Assignment1/spaceout/user/serializers.py
+++ Assignment/iscg7420-Assignment1/spaceout/user/serializers.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model, authenticate
+from core.models import Customer, OrderManager, User
 
 from rest_framework import serializers
 
@@ -20,6 +21,58 @@ class UserSerializer(serializers.ModelSerializer):
             user.set_password(password)
             user.save()
         return user
+
+
+class CustomerSerializer(serializers.ModelSerializer):
+    user = UserSerializer(required=False)
+
+    class Meta:
+        model = Customer
+        fields = ('user', 'first_name', 'last_name', 'phone_number', 'street_address',
+                  'suburb', 'city', 'country', 'postcode',)
+
+    def create(self, validated_data):
+        user = validated_data.pop('user')
+        user = User.objects.create(**user)
+        user.is_customer = True
+        user.save()
+        customer = Customer.objects.create(user=user, **validated_data)
+        return customer
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+        return user
+
+    # username = serializers.CharField(source='user.username')
+    # email = serializers.CharField(source='user.email')
+    # password = serializers.CharField(source='user.password')
+    # first_name = serializers.CharField(source='customer.first_name')
+    # last_name = serializers.CharField(source='customer.last_name')
+    # phone_number = serializers.CharField(source='customer.phone_number')
+    # street_address = serializers.CharField(source='customer.street_address')
+    # suburb = serializers.CharField(source='customer.suburb')
+    # city = serializers.CharField(source='customer.city')
+    # country = serializers.CharField(source='customer.country')
+    # postcode = serializers.CharField(source='customer.postcode')
+    #
+    # class Meta:
+    #     model = Customer
+    #     fields = ('username', 'email', 'password', 'first_name', 'last_name', 'phone_number', 'street_address',
+    #               'suburb', 'city', 'country', 'postcode',)
+    #
+    # def create(self, validated_data):
+    #     customer = validated_data.pop('customer')
+    #     new_user = validated_data.pop('user')
+    #     user = User.objects.create(**new_user)
+    #     user.set_password(validated_data['user.password'])
+    #     user.save()
+    #     Customer.objects.create(user=user, **customer)
+    #     return user
 
 
 class AuthTokenSerializer(serializers.Serializer):
