7a08ac36f55a107fc638b10e19765f5917f18b16
vBoBv <vannponhvath@gmail.com>
2020-10-29 01:37:41 +1300

    added PaymentViewSet

--- Assignment/iscg7420-Assignment1/spaceout/ecommerce/views.py
+++ Assignment/iscg7420-Assignment1/spaceout/ecommerce/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import viewsets, mixins
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from core.models import Payment
+
+from ecommerce import serializers
+
+
+class PaymentViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin):
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    queryset = Payment.objects.all()
+    serializer_class = serializers.PaymentSerializer
+
+    def get_queryset(self):
+        return self.queryset.filter(customer__user=self.request.user).order_by('-amount')
+
+    def perform_create(self, serializer):
+        serializer.save(customer=self.request.user.customer)
