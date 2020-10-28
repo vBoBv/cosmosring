4d368229f7b0128d6d9cc11209ea573194826692
vBoBv <vannponhvath@gmail.com>
2020-10-29 00:17:57 +1300

    added OrderManagerSerializer

--- Assignment/iscg7420-Assignment1/spaceout/user/serializers.py
+++ Assignment/iscg7420-Assignment1/spaceout/user/serializers.py
@@ -48,31 +48,30 @@ class CustomerSerializer(serializers.ModelSerializer):
             user.save()
         return user
 
-    # username = serializers.CharField(source='user.username')
-    # email = serializers.CharField(source='user.email')
-    # password = serializers.CharField(source='user.password')
-    # first_name = serializers.CharField(source='customer.first_name')
-    # last_name = serializers.CharField(source='customer.last_name')
-    # phone_number = serializers.CharField(source='customer.phone_number')
-    # street_address = serializers.CharField(source='customer.street_address')
-    # suburb = serializers.CharField(source='customer.suburb')
-    # city = serializers.CharField(source='customer.city')
-    # country = serializers.CharField(source='customer.country')
-    # postcode = serializers.CharField(source='customer.postcode')
-    #
-    # class Meta:
-    #     model = Customer
-    #     fields = ('username', 'email', 'password', 'first_name', 'last_name', 'phone_number', 'street_address',
-    #               'suburb', 'city', 'country', 'postcode',)
-    #
-    # def create(self, validated_data):
-    #     customer = validated_data.pop('customer')
-    #     new_user = validated_data.pop('user')
-    #     user = User.objects.create(**new_user)
-    #     user.set_password(validated_data['user.password'])
-    #     user.save()
-    #     Customer.objects.create(user=user, **customer)
-    #     return user
+
+class OrderManagerSerializer(serializers.ModelSerializer):
+    user = UserSerializer(required=False)
+
+    class Meta:
+        model = OrderManager
+        fields = ('user',)
+
+    def create(self, validated_data):
+        user = validated_data.pop('user')
+        user = User.objects.create_user(**user)
+        user.is_order_manager = True
+        user.save()
+        order_manager = OrderManager.objects.create(user=user)
+        return order_manager
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+        return user
 
 
 class AuthTokenSerializer(serializers.Serializer):
