d636a56f8baebe78377829f634a12218e7e079a2
vBoBv <vannponhvath@gmail.com>
2020-10-29 01:37:00 +1300

    added Payment url

---
+++ Assignment/iscg7420-Assignment1/spaceout/ecommerce/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from ecommerce import views
+from django.conf.urls.static import static
+from django.conf import settings
+
+router = DefaultRouter()
+router.register('payments', views.PaymentViewSet)
+
+app_name = 'ecommerce'
+
+urlpatterns = [
+    path('', include(router.urls))
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
