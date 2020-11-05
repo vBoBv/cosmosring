8b3fc1838099605ea8f70f79dc980de2cf6de668
vBoBv <vannponhvath@gmail.com>
2020-11-06 10:58:53 +1300

    setup CREATE, UPDATE, DELETE product action creators

--- Assignment/iscg7420-Assignment1/spaceout-client/src/actions/products.ts
+++ Assignment/iscg7420-Assignment1/spaceout-client/src/actions/products.ts
@@ -17,6 +17,26 @@ export interface IFetchProducts {
 	payload: IProduct[];
 }
 
+export interface IFetchProduct {
+	type: ActionTypes.fetchProduct;
+	payload: IProduct;
+}
+
+export interface ICreateProduct {
+	type: ActionTypes.createProduct;
+	payload: IProduct;
+}
+
+export interface IEditProduct {
+	type: ActionTypes.editProduct;
+	payload: IProduct;
+}
+
+export interface IDeleteProduct {
+	type: ActionTypes.deleteProduct;
+	payload: number;
+}
+
 export const fetchProducts = () => {
 	return async (dispatch: Dispatch) => {
 		const { data } = await Products.list();
@@ -24,6 +44,50 @@ export const fetchProducts = () => {
 		dispatch<IFetchProducts>({
 			type: ActionTypes.fetchProducts,
 			payload: data
+		});
+	};
+};
+
+export const fetchProduct = (id: number) => {
+	return async (dispatch: Dispatch) => {
+		const { data } = await Products.details(id);
+
+		dispatch<IFetchProduct>({
+			type: ActionTypes.fetchProduct,
+			payload: data
+		});
+	};
+};
+
+export const createProduct = (formValue: any) => {
+	return async (dispatch: Dispatch) => {
+		const { data } = await Products.create(formValue);
+
+		dispatch<ICreateProduct>({
+			type: ActionTypes.createProduct,
+			payload: data
+		});
+	};
+};
+
+export const editProduct = (id: number, formValue: any) => {
+	return async (dispatch: Dispatch) => {
+		const { data } = await Products.edit(id, formValue);
+
+		dispatch<IEditProduct>({
+			type: ActionTypes.editProduct,
+			payload: data
+		});
+	};
+};
+
+export const deleteProduct = (id: number) => {
+	return async (dispatch: Dispatch) => {
+		await Products.delete(id);
+
+		dispatch<IDeleteProduct>({
+			type: ActionTypes.deleteProduct,
+			payload: id
 		});
 	};
 };
