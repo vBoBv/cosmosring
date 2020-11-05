ef004e0f41fa401a2fe61b45c965394fb29a2995
vBoBv <vannponhvath@gmail.com>
2020-11-06 11:44:12 +1300

    refactored array-based reducers into object-based reducers

--- Assignment/iscg7420-Assignment1/spaceout-client/src/reducers/products.ts
+++ Assignment/iscg7420-Assignment1/spaceout-client/src/reducers/products.ts
@@ -1,11 +1,41 @@
-import { act } from 'react-dom/test-utils';
 import { IProduct, ActionTypes, Action } from '../actions';
+import _ from 'lodash';
+import { actionTypes } from 'redux-form';
 
-export const productsReducer = (state: IProduct[] = [], action: Action) => {
+// Object-based approach
+export const productsReducer = (state: IProduct = {} as IProduct, action: Action) => {
 	switch (action.type) {
 		case ActionTypes.fetchProducts:
-			return action.payload;
+			return { ...state, ..._.mapKeys(action.payload, 'id') };
+		case ActionTypes.fetchProduct:
+			return { ...state, [action.payload.id]: action.payload };
+		case ActionTypes.createProduct:
+			return { ...state, [action.payload.id]: action.payload };
+		case ActionTypes.editProduct:
+			return { ...state, [action.payload.id]: action.payload };
+		case ActionTypes.deleteProduct:
+			return _.omit(state, action.payload);
 		default:
 			return state;
 	}
 };
+
+// Array-based approach
+// export const productsReducer = (state: IProduct[] = [], action: Action) => {
+// 	switch (action.type) {
+// 		case ActionTypes.fetchProducts:
+// 			return action.payload;
+// 		case ActionTypes.fetchProduct:
+// 			return action.payload;
+// 		case ActionTypes.editProduct:
+// 			return state.map((product) => {
+// 				if (product.id === action.payload.id) {
+// 					return action.payload;
+// 				} else {
+// 					return product;
+// 				}
+// 			});
+// 		default:
+// 			return state;
+// 	}
+// };
