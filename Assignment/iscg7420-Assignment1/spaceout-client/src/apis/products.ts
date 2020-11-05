2b491f4c0fc78508213a11d733b49c03620a4e0b
vBoBv <vannponhvath@gmail.com>
2020-11-06 11:00:15 +1300

    added create, update, delete api requests for product

--- Assignment/iscg7420-Assignment1/spaceout-client/src/apis/products.ts
+++ Assignment/iscg7420-Assignment1/spaceout-client/src/apis/products.ts
@@ -11,9 +11,17 @@ axios.defaults.baseURL =
 		: 'http://localhost:8000/api/ecommerce/';
 
 const requests = {
-	getAll: (url: string) => axios.get<IProduct[]>(url)
+	getAll: (url: string) => axios.get<IProduct[]>(url),
+	get: (url: string) => axios.get<IProduct>(url),
+	create: (url: string, formValues: any) => axios.post<IProduct>(url, formValues),
+	edit: (url: string, formValues: any) => axios.patch<IProduct>(url, formValues),
+	delete: (url: string) => axios.delete<number>(url)
 };
 
 export const Products = {
-	list: () => requests.getAll('/products/')
+	list: () => requests.getAll('/products/'),
+	details: (id: number) => requests.get(`/products/${id}`),
+	create: (formValues: any) => requests.create('/products/', { ...formValues }),
+	edit: (id: number, formValues: any) => requests.edit(`/products/${id}`, formValues),
+	delete: (id: number) => requests.delete(`/products/${id}`)
 };
