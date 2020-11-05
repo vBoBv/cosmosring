29cf6a13e1cd7000b9ad1cfeb8ae9703512ca448
vBoBv <vannponhvath@gmail.com>
2020-11-06 02:02:03 +1300

    setup a GET request to get all products

--- Assignment/iscg7420-Assignment1/spaceout-client/src/apis/products.ts
+++ Assignment/iscg7420-Assignment1/spaceout-client/src/apis/products.ts
@@ -1,5 +1,19 @@
 import axios from 'axios';
+import { IProduct } from '../actions/products';
 
-export default axios.create({
-	baseURL: 'http://127.0.0.1:8000/api/ecommerce/'
-});
+// export default axios.create({
+// 	baseURL: 'http://127.0.0.1:8000/api/ecommerce/products/'
+// });
+
+axios.defaults.baseURL =
+	process.env.NODE_ENV === 'development'
+		? 'http://localhost:8000/api/ecommerce/'
+		: 'http://localhost:8000/api/ecommerce/';
+
+const requests = {
+	getAll: (url: string) => axios.get<IProduct[]>(url)
+};
+
+export const Products = {
+	list: () => requests.getAll('/products')
+};
