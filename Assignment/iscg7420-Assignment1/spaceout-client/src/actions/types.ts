bca3bb238504de3e9ef6f3f4420a0ddd478f2c0f
vBoBv <vannponhvath@gmail.com>
2020-11-06 16:05:57 +1300

    added loginUser action types

--- Assignment/iscg7420-Assignment1/spaceout-client/src/actions/types.ts
+++ Assignment/iscg7420-Assignment1/spaceout-client/src/actions/types.ts
@@ -1,11 +1,13 @@
 import { IFetchProducts, IFetchProduct, ICreateProduct, IEditProduct, IDeleteProduct } from './products';
+import { ILoginUser } from './users';
 
 export enum ActionTypes {
 	fetchProducts,
 	fetchProduct,
 	createProduct,
 	editProduct,
-	deleteProduct
+	deleteProduct,
+	loginUser
 }
 
-export type Action = IFetchProducts | IFetchProduct | ICreateProduct | IEditProduct | IDeleteProduct;
+export type Action = IFetchProducts | IFetchProduct | ICreateProduct | IEditProduct | IDeleteProduct | ILoginUser;
