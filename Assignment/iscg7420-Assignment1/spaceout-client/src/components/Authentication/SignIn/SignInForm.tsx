c46616f19bd8d24981c053577def42514b79f155
vBoBv <vannponhvath@gmail.com>
2020-11-06 16:30:30 +1300

    passed back formValues to onSignIn to login user

--- Assignment/iscg7420-Assignment1/spaceout-client/src/components/Authentication/SignIn/SignInForm.tsx
+++ Assignment/iscg7420-Assignment1/spaceout-client/src/components/Authentication/SignIn/SignInForm.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import { Grid, Button, Typography } from '@material-ui/core';
-import { Field, reduxForm } from 'redux-form';
+import { Field, reduxForm, InjectedFormProps } from 'redux-form';
 import { renderTextField } from '../../FormInputs/FormInputs';
 import { useStyles } from '../AuthenticationCSS';
 import { getSignInAccountDetailFields } from './SignInStaticContent';
+import { ILoginForm } from '../../../actions';
 
 import LockIcon from '@material-ui/icons/Lock';
 import EmailIcon from '@material-ui/icons/Email';
 
-const SignInForm = () => {
+interface ISignInFormProps {
+	onSignIn: (formValues: ILoginForm) => void;
+}
+
+const SignInForm: React.FC<InjectedFormProps<{}, ISignInFormProps> & ISignInFormProps> = ({
+	handleSubmit,
+	onSignIn
+}) => {
 	const { instructions, fieldContainer, fieldItem } = useStyles();
 
 	const signInAccountDetailFields = getSignInAccountDetailFields();
@@ -32,19 +40,24 @@ const SignInForm = () => {
 		);
 	});
 
+	// TODO: ILoginForm type, SHOULD NOT BE any, gives compile error to handleSubmit, DO MORE REASEARCH ON the type of formValues provided by redux-form
+	const onSubmit = (formValues: any) => {
+		onSignIn(formValues);
+	};
+
 	const renderSignInForm = () => {
 		return (
-			<div>
+			<form onSubmit={handleSubmit(onSubmit)}>
 				<Typography className={instructions} variant='body2'>
 					Fill in your details!
 				</Typography>
 				{renderSignInFields}
 				<Grid item>
-					<Button variant='contained' color='secondary'>
+					<Button variant='contained' color='secondary' type='submit'>
 						Sign In
 					</Button>
 				</Grid>
-			</div>
+			</form>
 		);
 	};
 
@@ -55,7 +68,7 @@ const SignInForm = () => {
 	);
 };
 
-export default reduxForm({
+export default reduxForm<{}, ISignInFormProps>({
 	form: 'signInForm',
 	destroyOnUnmount: false
 })(SignInForm);
