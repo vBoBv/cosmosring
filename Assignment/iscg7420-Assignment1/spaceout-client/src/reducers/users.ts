966fb75b41e99c9ed5bf6059a027666b814d5cec
vBoBv <vannponhvath@gmail.com>
2020-11-11 15:40:56 +1300

    setup reducer to return the customer info after signed up

--- Assignment/iscg7420-Assignment1/spaceout-client/src/reducers/users.ts
+++ Assignment/iscg7420-Assignment1/spaceout-client/src/reducers/users.ts
@@ -5,6 +5,8 @@ export const usersReducer = (state: IUser = {} as IUser, action: Action) => {
 	switch (action.type) {
 		case ActionTypes.loginUser:
 			return { ...state, payload: action.payload };
+		case ActionTypes.signUpCustomer:
+			return { ...state, payload: action.payload };
 		default:
 			return state;
 	}
