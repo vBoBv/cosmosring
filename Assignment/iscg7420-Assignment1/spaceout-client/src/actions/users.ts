83023ea788b33b920817fc4320de1722ec05428f
vBoBv <vannponhvath@gmail.com>
2020-11-11 15:39:38 +1300

    added signUpCustomer action creator

--- Assignment/iscg7420-Assignment1/spaceout-client/src/actions/users.ts
+++ Assignment/iscg7420-Assignment1/spaceout-client/src/actions/users.ts
@@ -8,6 +8,28 @@ export interface ILoginForm {
 	password: string;
 }
 
+export interface ICustomerForm {
+	email: string;
+	password: string;
+	username: string;
+	first_name: string;
+	last_name: string;
+	phone_number: string;
+	street_address: string;
+	suburb: string;
+	city: string;
+	country: string;
+	postcode: number;
+}
+
+export interface IOrderManagerForm {
+	user: {
+		email: string;
+		password: string;
+		username: string;
+	};
+}
+
 export interface IToken {
 	token: string;
 }
@@ -17,14 +39,35 @@ export interface IUser {
 	username: string;
 }
 
+export interface ICustomer {
+	user: {
+		email: string;
+		password: string;
+		username: string;
+	};
+	first_name: string;
+	last_name: string;
+	phone_number: string;
+	street_address: string;
+	suburb: string;
+	city: string;
+	country: string;
+	postcode: number;
+}
+
 export interface ILoginUser {
 	type: ActionTypes.loginUser;
 	payload: IUser;
 }
 
-export const loginUser = (formValue: ILoginForm) => {
+export interface ISignUpCustomer {
+	type: ActionTypes.signUpCustomer;
+	payload: IUser;
+}
+
+export const loginUser = (formValues: ILoginForm) => {
 	return async (dispatch: Dispatch) => {
-		const { data: responseToken } = await Users.retrieveToken(formValue);
+		const { data: responseToken } = await Users.retrieveToken(formValues);
 		const authToken = responseToken.token;
 
 		window.localStorage.setItem('authToken', authToken);
@@ -33,6 +76,55 @@ export const loginUser = (formValue: ILoginForm) => {
 
 		dispatch<ILoginUser>({
 			type: ActionTypes.loginUser,
+			payload: data
+		});
+
+		history.push('/');
+	};
+};
+
+export const signUpCustomer = ({
+	email,
+	password,
+	username,
+	first_name,
+	last_name,
+	phone_number,
+	street_address,
+	suburb,
+	city,
+	country,
+	postcode
+}: ICustomerForm) => {
+	const submittedFormValue = {
+		user: {
+			email: email,
+			password: password,
+			username: username
+		},
+		first_name: first_name,
+		last_name: last_name,
+		phone_number: phone_number,
+		street_address: street_address,
+		suburb: suburb,
+		city: city,
+		country: country,
+		postcode: postcode
+	};
+
+	return async (dispatch: Dispatch) => {
+		const { data: customerInfo } = await Users.signUpCustomer(submittedFormValue);
+		const { data: responseToken } = await Users.retrieveToken({
+			email: customerInfo.user.email,
+			password: password
+		});
+		const authToken = responseToken.token;
+		window.localStorage.setItem('authToken', authToken);
+
+		const { data } = await Users.login();
+
+		dispatch<ISignUpCustomer>({
+			type: ActionTypes.signUpCustomer,
 			payload: data
 		});
 
