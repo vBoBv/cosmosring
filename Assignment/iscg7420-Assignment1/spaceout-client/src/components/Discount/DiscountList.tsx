49154ea83d49f9484fb8ebdaf479532de0d3aa77
vBoBv <vannponhvath@gmail.com>
2020-11-12 14:57:15 +1300

    modified DiscountList to render a list of discount codes

--- Assignment/iscg7420-Assignment1/spaceout-client/src/components/Discount/DiscountList.tsx
+++ Assignment/iscg7420-Assignment1/spaceout-client/src/components/Discount/DiscountList.tsx
@@ -1,8 +1,51 @@
 import React, { Component } from 'react';
+import { IDiscount } from '../../actions';
+import {
+	List,
+	ListItem,
+	ListItemText,
+	ListItemAvatar,
+	ListItemSecondaryAction,
+	Avatar,
+	IconButton,
+	Grid
+} from '@material-ui/core';
+import ConfirmationNumberIcon from '@material-ui/icons/ConfirmationNumber';
+import DeleteIcon from '@material-ui/icons/Delete';
 
-class DiscountList extends Component {
+interface IDiscountListProps {
+	discounts: IDiscount[];
+}
+
+class DiscountList extends Component<IDiscountListProps> {
+	renderDiscounts() {
+		return this.props.discounts.map(({ id, discount_code }) => {
+			return (
+				<ListItem key={id}>
+					<ListItemAvatar>
+						<Avatar>
+							<ConfirmationNumberIcon />
+						</Avatar>
+					</ListItemAvatar>
+					<ListItemText primary={discount_code} />
+					<ListItemSecondaryAction>
+						<IconButton edge='end' aria-label='delete'>
+							<DeleteIcon />
+						</IconButton>
+					</ListItemSecondaryAction>
+				</ListItem>
+			);
+		});
+	}
+
 	render() {
-		return <div>DiscountList</div>;
+		return (
+			<Grid item>
+				<List component='nav' aria-label='secondary mailbox folders'>
+					{this.renderDiscounts()}
+				</List>
+			</Grid>
+		);
 	}
 }
 
