6b5d964c5a46282bb871cffa20a6f687d608cd29
vBoBv <vannponhvath@gmail.com>
2020-11-13 00:45:32 +1300

    added ProductFormStaticContent

---
+++ Assignment/iscg7420-Assignment1/spaceout-client/src/components/ShoppingDrawer/Product/ProductFormStaticContent.ts
@@ -0,0 +1,10 @@
+export const getProductFields = () => {
+	return [
+		{ name: 'name', label: 'Name' },
+		{ name: 'price', label: 'Price' },
+		{ name: 'description', label: 'Description' }
+		// { name: 'image', label: 'Image' },
+		// { name: 'category', label: 'Category' },
+		// { name: 'discount', label: 'Discount' }
+	];
+};
