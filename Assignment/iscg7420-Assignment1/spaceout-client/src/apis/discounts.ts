1bad4581e2a6cb5c47d2474e78fc24dd5e7a0ca2
vBoBv <vannponhvath@gmail.com>
2020-11-12 13:57:14 +1300

    setup Discount api end points

---
+++ Assignment/iscg7420-Assignment1/spaceout-client/src/apis/discounts.ts
@@ -0,0 +1,25 @@
+import axios from 'axios';
+import { IDiscount } from '../actions/discounts';
+
+const discounts = axios.create({
+	baseURL:
+		process.env.NODE_ENV === 'development'
+			? 'http://localhost:8000/api/ecommerce/'
+			: 'http://localhost:8000/api/ecommerce/'
+});
+
+const requests = {
+	getAll: (url: string) => discounts.get<IDiscount[]>(url),
+	get: (url: string) => discounts.get<IDiscount>(url),
+	create: (url: string, formValues: IDiscount) => discounts.post<IDiscount>(url, formValues),
+	edit: (url: string, formValues: IDiscount) => discounts.patch<IDiscount>(url, formValues),
+	delete: (url: string) => discounts.delete<number>(url)
+};
+
+export const Discounts = {
+	list: () => requests.getAll('/discounts/'),
+	details: (id: number) => requests.get(`/discounts/${id}`),
+	create: (formValues: IDiscount) => requests.create('/discounts/', { ...formValues }),
+	edit: (id: number, formValues: IDiscount) => requests.edit(`/discounts/${id}`, formValues),
+	delete: (id: number) => requests.delete(`/discounts/${id}`)
+};
