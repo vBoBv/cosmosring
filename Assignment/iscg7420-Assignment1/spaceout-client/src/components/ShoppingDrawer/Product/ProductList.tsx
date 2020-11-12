211584570619d3a9fb8168d068e7175964457309
vBoBv <vannponhvath@gmail.com>
2020-11-13 03:48:02 +1300

    setup a function to retreive products from sessionStorage

--- Assignment/iscg7420-Assignment1/spaceout-client/src/components/ShoppingDrawer/Product/ProductList.tsx
+++ Assignment/iscg7420-Assignment1/spaceout-client/src/components/ShoppingDrawer/Product/ProductList.tsx
@@ -25,10 +25,11 @@ const ProductList: React.FC<IProductListProps> = ({ products }) => {
 
 	const onDelete = (id: number) => {
 		dispatch(deleteProduct(id));
+		window.location.reload();
 	};
 
-	const renderProducts = () => {
-		return products.map(({ id, name }) => {
+	const renderProductList = (productList: IProduct[]) => {
+		return productList.map(({ id, name }) => {
 			return (
 				<ListItem key={id}>
 					<ListItemAvatar>
@@ -55,6 +56,16 @@ const ProductList: React.FC<IProductListProps> = ({ products }) => {
 		});
 	};
 
+	const renderProducts = () => {
+		const sessionStorageProducts = window.sessionStorage.getItem('products');
+		if (sessionStorageProducts) {
+			const retreivedStorageProducts: IProduct[] = JSON.parse(sessionStorageProducts);
+			return renderProductList(retreivedStorageProducts);
+		} else {
+			return renderProductList(products);
+		}
+	};
+
 	return (
 		<Grid item>
 			<List component='nav' aria-label='secondary mailbox folders'>
@@ -65,54 +76,3 @@ const ProductList: React.FC<IProductListProps> = ({ products }) => {
 };
 
 export default ProductList;
-
-// interface IDiscountListProps {
-// 	discounts: IDiscount[];
-// 	deleteDiscount: (id: number) => void;
-// }
-
-// class DiscountList extends Component<IDiscountListProps> {
-// 	onDelete = (id: number) => {
-// 		this.props.deleteDiscount(id);
-// 	};
-
-// 	renderDiscounts() {
-// 		return this.props.discounts.map(({ id, discount_code }) => {
-// 			return (
-// 				<ListItem key={id}>
-// 					<ListItemAvatar>
-// 						<Avatar>
-// 							<ConfirmationNumberIcon />
-// 						</Avatar>
-// 					</ListItemAvatar>
-// 					<Link to={`/discounts/${id}`} style={{ textDecoration: 'none', color: 'white' }}>
-// 						<ListItemText primary={discount_code} />
-// 					</Link>
-// 					<ListItemSecondaryAction>
-// 						<Link to={`/discounts/edit/${id}`} style={{ textDecoration: 'none' }}>
-// 							<IconButton edge='start' color='primary'>
-// 								<EditIcon />
-// 							</IconButton>
-// 						</Link>
-
-// 						<IconButton edge='end' color='secondary' onClick={() => this.onDelete(id)}>
-// 							<DeleteIcon />
-// 						</IconButton>
-// 					</ListItemSecondaryAction>
-// 				</ListItem>
-// 			);
-// 		});
-// 	}
-
-// 	render() {
-// 		return (
-// 			<Grid item>
-// 				<List component='nav' aria-label='secondary mailbox folders'>
-// 					{this.renderDiscounts()}
-// 				</List>
-// 			</Grid>
-// 		);
-// 	}
-// }
-
-// export default connect(null, { deleteDiscount })(DiscountList);
