099ac10c08eecf19ed825511309a6df01ece5f4f
vBoBv <vannponhvath@gmail.com>
2020-11-12 18:11:01 +1300

    wired up mapStateToProps, fetchDiscount and editDiscount and pass it to DiscountForm

--- Assignment/iscg7420-Assignment1/spaceout-client/src/components/Discount/DiscountEdit.tsx
+++ Assignment/iscg7420-Assignment1/spaceout-client/src/components/Discount/DiscountEdit.tsx
@@ -1,9 +1,55 @@
 import React, { Component } from 'react';
+import { RouteComponentProps } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { fetchDiscount, editDiscount } from '../../actions/discounts';
+import { StoreState } from '../../reducers';
+import { IDiscount } from '../../actions';
+import DiscountForm from './DiscountForm';
+import _ from 'lodash';
 
-class DiscountEdit extends Component {
+interface IDiscountEditProps {
+	discounts: IDiscount[];
+	fetchDiscount: (id: number) => void;
+	editDiscount: (id: number, formValues: any) => void;
+}
+
+interface IRouteComponentMatchParamProps {
+	id: string;
+}
+
+class DiscountEdit extends Component<RouteComponentProps<IRouteComponentMatchParamProps> & IDiscountEditProps> {
+	componentDidMount() {
+		const { id } = this.props.match.params;
+
+		this.props.fetchDiscount(parseInt(id));
+	}
+
+	onSubmit = (formValues: any) => {
+		const { id } = this.props.match.params;
+
+		this.props.editDiscount(parseInt(id), formValues);
+	};
+
 	render() {
-		return <div>DiscountEdit</div>;
+		return (
+			<div style={{ backgroundColor: 'grey', paddingTop: '5rem' }}>
+				<h1>Edit Discount</h1>
+				<DiscountForm
+					initialValues={_.pick(
+						this.props.discounts.filter((discount) => discount.id === parseInt(this.props.match.params.id))[0],
+						'discount_code'
+					)}
+					onSubmit={this.onSubmit}
+				/>
+			</div>
+		);
 	}
 }
 
-export default DiscountEdit;
+const mapStateToProps = ({ discounts }: StoreState): { discounts: IDiscount[] } => {
+	return {
+		discounts: Object.values(discounts)
+	};
+};
+
+export default connect(mapStateToProps, { fetchDiscount, editDiscount })(DiscountEdit);
