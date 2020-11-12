35189d2b16fdd88807d4c7628ee668b2ec3712e2
vBoBv <vannponhvath@gmail.com>
2020-11-12 17:02:25 +1300

    wired up mapStateToProps and fetchDiscount action creator to render a discound code

--- Assignment/iscg7420-Assignment1/spaceout-client/src/components/Discount/DiscountShow.tsx
+++ Assignment/iscg7420-Assignment1/spaceout-client/src/components/Discount/DiscountShow.tsx
@@ -1,9 +1,54 @@
 import React, { Component } from 'react';
+import { IDiscount } from '../../actions';
+import { RouteComponentProps } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { fetchDiscount } from '../../actions/discounts';
+import { StoreState } from '../../reducers';
+import { Typography, Grid } from '@material-ui/core';
 
-class DiscountShow extends Component {
+interface IDiscountShowProps {
+	discounts: IDiscount[];
+	fetchDiscount: (id: number) => void;
+}
+
+interface IRouteComponentMatchParamProps {
+	id: string;
+}
+
+class DiscountShow extends Component<RouteComponentProps<IRouteComponentMatchParamProps> & IDiscountShowProps> {
+	componentDidMount() {
+		const { id } = this.props.match.params;
+
+		this.props.fetchDiscount(parseInt(id));
+	}
+
+	renderDiscounts() {
+		const { id } = this.props.match.params;
+		const discount = this.props.discounts.filter((discount) => discount.id === parseInt(id));
+
+		return discount.map(({ id, discount_code, is_expired }) => {
+			return (
+				<Grid item key={id}>
+					<Typography>Discount code: {discount_code}</Typography>
+					<Typography>Is expired: {is_expired ? 'Yes' : 'No'}</Typography>
+				</Grid>
+			);
+		});
+	}
+
 	render() {
-		return <div>DiscountShow</div>;
+		return (
+			<Grid container style={{ paddingTop: '5rem', backgroundColor: 'grey' }}>
+				{this.renderDiscounts()}
+			</Grid>
+		);
 	}
 }
 
-export default DiscountShow;
+const mapStateToProps = ({ discounts }: StoreState): { discounts: IDiscount[] } => {
+	return {
+		discounts: Object.values(discounts)
+	};
+};
+
+export default connect(mapStateToProps, { fetchDiscount })(DiscountShow);
