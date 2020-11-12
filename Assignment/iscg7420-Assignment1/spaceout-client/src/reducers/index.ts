91f0b0ab8e2852c74bdcdeee3c0dd7ed95afeb53
vBoBv <vannponhvath@gmail.com>
2020-11-12 14:55:43 +1300

    added discountsReducer to combineReducers

--- Assignment/iscg7420-Assignment1/spaceout-client/src/reducers/index.ts
+++ Assignment/iscg7420-Assignment1/spaceout-client/src/reducers/index.ts
@@ -1,17 +1,20 @@
 import { combineReducers } from 'redux';
 import { reducer as reduxForm, FormStateMap } from 'redux-form';
-import { IUser, IProduct } from '../actions';
+import { IUser, IProduct, IDiscount } from '../actions';
 import { productsReducer } from './products';
 import { usersReducer } from './users';
+import { discountsReducer } from './discounts';
 
 export interface StoreState {
 	form: FormStateMap;
-	products: IProduct;
 	users: IUser;
+	products: IProduct;
+	discounts: IDiscount;
 }
 
 export const reducers = combineReducers<StoreState>({
 	form: reduxForm,
+	users: usersReducer,
 	products: productsReducer,
-	users: usersReducer
+	discounts: discountsReducer
 });
