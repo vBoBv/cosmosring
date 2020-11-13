ff0e960d4d5eee47599dd4c471c3290e712be16e
vBoBv <vannponhvath@gmail.com>
2020-11-13 15:15:17 +1300

    removed null=True on discount and category for Product

--- Assignment/iscg7420-Assignment1/spaceout/core/models.py
+++ Assignment/iscg7420-Assignment1/spaceout/core/models.py
@@ -125,8 +125,8 @@ class Product(models.Model):
     price = models.FloatField()
     image = models.ImageField(null=True, blank=True)
     description = models.TextField(blank=True)
-    category = models.ManyToManyField('Category', null=True, blank=True)
-    discount = models.ManyToManyField('Discount', null=True, blank=True)
+    category = models.ManyToManyField('Category', blank=True)
+    discount = models.ManyToManyField('Discount', blank=True)
 
     def __str__(self):
         return self.name
