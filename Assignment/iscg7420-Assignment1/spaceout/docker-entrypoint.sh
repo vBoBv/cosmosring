808de486a0aa13cd84dafea5e5fa821d31049b4b
vBoBv <vannponhvath@gmail.com>
2020-11-17 14:44:50 +1300

    added aws postgres env variables and run migrations

--- Assignment/iscg7420-Assignment1/spaceout/docker-entrypoint.sh
+++ Assignment/iscg7420-Assignment1/spaceout/docker-entrypoint.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
+
+# Collect static files
+python manage.py collectstatic --noinput
+
+# Database migrations
+python manage.py migrate
+python manage.py makemigrations core
+python manage.py migrate
+
 exec gunicorn --bind :8000 --workers 3 spaceout.wsgi:application
