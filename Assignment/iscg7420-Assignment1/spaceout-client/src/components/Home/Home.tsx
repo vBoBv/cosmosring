7bcc82a31526c0611213ef2981f70095b5356030
vBoBv <vannponhvath@gmail.com>
2020-11-19 02:15:20 +1300

    implemented userEffect to fetch user if there is authToken

--- Assignment/iscg7420-Assignment1/spaceout-client/src/components/Home/Home.tsx
+++ Assignment/iscg7420-Assignment1/spaceout-client/src/components/Home/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Grid } from '@material-ui/core';
 import { useStyles } from './HomeCSS';
 import ReactPageScroller from 'react-page-scroller';
@@ -6,15 +6,39 @@ import HomeLandingContent from './HomeLandingContent';
 import HomeLandingAstronaut from './HomeLandingAstronaut';
 import StarParticles from '../StarParticles/StarParticles';
 import { IUser } from '../../actions';
+import { useSelector, shallowEqual } from 'react-redux';
+import { StoreState } from '../../reducers';
 
-interface IHomeProps {
-	account: IUser | null;
-	setAccount: (account: IUser | null) => void;
-}
-
-const Home: React.FC<IHomeProps> = ({ account, setAccount }) => {
+const Home = () => {
 	const { containerBackground } = useStyles();
 	const [currentPage, setCurrentPage] = useState<number>(0);
+
+	const [account, setAccount] = useState<IUser | null>(null);
+	const [authToken] = useState<String | null>(window.localStorage.getItem('authToken'));
+
+	const user: IUser = useSelector(({ users }: StoreState) => Object.values(users), shallowEqual)[0];
+	const API_URL =
+		process.env.NODE_ENV === 'development'
+			? 'http://localhost:8000/api/user/profile/'
+			: 'http://ec2-3-25-136-35.ap-southeast-2.compute.amazonaws.com/api/user/profile/';
+
+	useEffect(() => {
+		if (authToken) {
+			fetch(API_URL, {
+				method: 'GET',
+				headers: {
+					'Content-Type': 'application/json',
+					Authorization: `Token ${window.localStorage.getItem('authToken')}`
+				}
+			})
+				.then((response) => response.json())
+				.then((response) => {
+					setAccount(response);
+				});
+		} else {
+			return;
+		}
+	}, [authToken, user, API_URL]);
 
 	const handlePageChange = (pageNumber: number) => setCurrentPage(pageNumber);
 
